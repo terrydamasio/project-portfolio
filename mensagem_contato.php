2327e98c2a3494cba4bc13cf49cd98debdba3577
Terry Damasio <terrydamasio610@gmail.com>
2023-08-25 13:48:36 -0300

    Atualização

--- mensagem_contato.php
+++ mensagem_contato.php
@@ -2,16 +2,16 @@
 
     $nome = addslashes($_POST['nome']);
     $telefone = addslashes($_POST['telefone']);
-    $assunto = addslashes($_POST['assunto']);
     $mensagem = addslashes($_POST['mensagem']);
 
-    $destinatario = "terrydamasio.dev@gmail.com";
-    
-    $corpo = "Nome: " . $nome . "\n" . "Telefone: " . $telefone . "\n" . "Assunto: " . $assunto . "\n" . "Mensagem: " . $mensagem;
+    $para = "terrydamasio.dev@gmail.com";
+    $assunto = "Trabalho - Formulário Portfolio";
 
-    $cabecalho = "From: " . $nome . "\n" . "Reply-top: " . $destinatario . "\n" . "X=Mailer:PHP/" . phpversion();
+    $corpo = "Nome: " . $nome . "\n" . "Telefone: " . $telefone . "\n" . "Mensagem: " . $mensagem;
 
-    if(mail($destinatario, $assunto, $corpo, $cabecalho)) {
+    $cabeca = "From: teste@damasio.com" . "\n" . "Reply-top: " . $para . "\n" . "X=Mailer:PHP/" . phpversion();
+
+    if(mail($para, $assunto, $corpo, $cabeca)) {
         echo "Email enviado";
     } else {
         echo "Erro ao enviar email"; 
